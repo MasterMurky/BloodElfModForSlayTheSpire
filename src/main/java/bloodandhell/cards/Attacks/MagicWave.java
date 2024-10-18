7efd69c9f8a7bdb38d6cdc34e49f300451152d5b
Charles Etienne <charles.etienne@student.unamur.be>
2024-10-18 21:47:39 +0100

    Refactor OutBurstOfAnger in MagicWave

--- src/main/java/bloodandhell/cards/Attacks/MagicWave.java
+++ src/main/java/bloodandhell/cards/Attacks/MagicWave.java
@@ -5,52 +5,64 @@ import bloodandhell.character.MyCharacter;
 import bloodandhell.util.CardStats;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.ShockWaveEffect;
 
-public class OutburstOfAnger extends BaseCard {
-    public static final String ID = makeID(OutburstOfAnger.class.getSimpleName());
+public class MagicWave extends BaseCard {
+    public static final String ID = makeID(MagicWave.class.getSimpleName());
     private static final CardStats info = new CardStats(
             MyCharacter.Enums.CARD_COLOR,
             CardType.ATTACK,
             CardRarity.COMMON,
             CardTarget.ALL_ENEMY,
-            3 // Coût initial de la carte
+            3
     );
 
-    public OutburstOfAnger() {
+    public MagicWave() {
         super(ID, info);
-        this.isMultiDamage = true; // Indique que la carte inflige des dégâts à tous les ennemis
-        this.baseDamage = GameActionManager.damageReceivedThisCombat; // Initialise la base des dégâts
+        this.isMultiDamage = true; // MultiDMG
+        this.baseDamage = GameActionManager.damageReceivedThisCombat; // Initialize
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        // Dégâts égaux aux dégâts subis par le joueur pendant ce combat
+        // Variable
         int damageTaken = GameActionManager.damageReceivedThisCombat;
         this.baseDamage = damageTaken;
 
-        // Infliger des dégâts à tous les ennemis
+        // Visuals
+        addToBot((AbstractGameAction)new SFXAction("ATTACK_PIERCING_WAIL"));
+        if (Settings.FAST_MODE) {
+            addToBot((AbstractGameAction)new VFXAction((AbstractCreature)p, (AbstractGameEffect)new ShockWaveEffect(p.hb.cX, p.hb.cY, Settings.PURPLE_COLOR, ShockWaveEffect.ShockWaveType.CHAOTIC), 0.3F));
+        } else {
+            addToBot((AbstractGameAction)new VFXAction((AbstractCreature)p, (AbstractGameEffect)new ShockWaveEffect(p.hb.cX, p.hb.cY, Settings.PURPLE_COLOR, ShockWaveEffect.ShockWaveType.CHAOTIC), 1.5F));
+        }
+
+        // Damage all the ennemies
         addToBot(new DamageAllEnemiesAction(
                 p,
                 DamageInfo.createDamageMatrix(damageTaken, true),
                 DamageInfo.DamageType.NORMAL,
-                AbstractGameAction.AttackEffect.SLASH_VERTICAL
+                AbstractGameAction.AttackEffect.NONE
         ));
+
     }
 
     @Override
     public void applyPowers() {
-        // Met à jour les dégâts basés sur les dégâts subis pendant le combat
         this.baseDamage = GameActionManager.damageReceivedThisCombat;
         super.applyPowers();
 
-        // Met à jour la description pour afficher les dégâts dynamiquement
+        // Dynamic description
         this.rawDescription = "Deal (" + this.baseDamage + ") damage to ALL enemies. Damage equal to the HP lost this combat.";
         initializeDescription();
     }
@@ -59,12 +71,12 @@ public class OutburstOfAnger extends BaseCard {
     public void upgrade() {
         if (!this.upgraded) {
             upgradeName();
-            upgradeBaseCost(2); // Réduire le coût de la carte en cas d'amélioration
+            upgradeBaseCost(2);
         }
     }
 
     @Override
     public AbstractCard makeCopy() {
-        return new OutburstOfAnger();
+        return new MagicWave();
     }
 }
